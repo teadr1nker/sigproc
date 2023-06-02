617a414dc01d4e5614089c2402510bd4f583cae5
Juhani <arsmir999@gmail.com>
2023-06-03 01:53:55 +0300

    t2

---
+++ task2/file2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+def X1(t):
+    return np.exp(-(t ** 2))
+
+def X2(T):
+    return [np.cos((np.pi * t) / 2) if np.abs(t) <=1 else 0 for t in T]
+
+def ampSpec(f):
+    T = .01
+    t = np.arange(1, 5, T)
+    x = f(t)
+    plt.plot(t, x)
+    plt.show()
+    plt.clf()
+
+    xFFT = np.fft.fft(x)
+    freq = np.fft.fftfreq(len(x), 100)
+    amp = np.abs(xFFT)
+    plt.plot(freq, amp)
+    plt.show()
+
+ampSpec(X2)
